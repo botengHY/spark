3d78d207aa871023e357f1fc668107875f1a0147
Bo Teng <boteng1@illinois.edu>
2017-03-24 13:20:51 -0500

    remove  println

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/KMeans.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/KMeans.scala
@@ -307,14 +307,8 @@ class KMeans private (
 
       var durationRatio = (ret._1.minBy(_._2)._2).toDouble/(ret._1.maxBy(_._2)._2).toDouble
 
-      println("curr locWeight is ", ret._2)
-      println("durationMap is ", ret._1)
-      println("durationRatio is ", durationRatio)
-      println("prev locWeight is ", prevlocWeight)
-
       if(durationRatio < 0.7){
         if(iteration - ephemeral == 1){
-          println("****repartitionWithWeight")
           ephemeral = 0
           prevlocWeight = ret._2
           rdd = rdd.repartitionWithWeight(ret._2)
